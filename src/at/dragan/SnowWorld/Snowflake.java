62af68e3d92776d0a386691d7ebd156558559819
Dragan-Nikolic67 <nesamskolovao2@gmail.com>
2024-05-15 08:45:02 +0200

    Finished Snowworld, maybe possible to improve spawnpoints

--- src/at/dragan/SnowWorld/Snowflake.java
+++ src/at/dragan/SnowWorld/Snowflake.java
@@ -4,22 +4,23 @@ import org.newdawn.slick.Graphics;
 import java.util.Random;
 public class Snowflake implements Actor {
     public enum SIZE {Large, Medium, Small};
-    private int x,y,speed,size;
+    private int x,y,size;
+    private double speed;
     private Random random;
     public Snowflake (SIZE size){
         this.random = new Random();
 
         if (size == SIZE.Large){
-            this.size = 12;
-            this.speed = 2;
+            this.size = 13;
+            this.speed =1.5;
         }
         if (size == SIZE.Medium){
             this.size = 8;
-            this.speed = 5;
+            this.speed = 1;
         }
         if (size == SIZE.Small){
             this.size = 4;
-            this.speed = 8;
+            this.speed = 0.5;
         }
 
         setRandomPosition();
@@ -27,7 +28,7 @@ public class Snowflake implements Actor {
 
     private void setRandomPosition(){
         this.x = this.random.nextInt(800);
-        this.y = this.random.nextInt(600) - 600;
+        this.y = this.random.nextInt(600) - 900;
     }
 
     @Override
@@ -35,7 +36,7 @@ public class Snowflake implements Actor {
         graphics.fillOval(this.x, this.y, this.size, this.size);
     }
     public void update (int delta){
-        this.y += delta/this.speed;
+        this.y += (double) delta/this.speed;
         if (this.y>600){
             setRandomPosition();
         }
--- src/at/dragan/SnowWorld/Snowworld.java
+++ src/at/dragan/SnowWorld/Snowworld.java
@@ -1,34 +1,49 @@
 package at.dragan.SnowWorld;
 
-import at.dragan.firstgame.ObjectsGame;
 import org.newdawn.slick.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Snowworld extends BasicGame {
+    private List<Actor> snowflakes;
+
     public Snowworld(String title) {
         super(title);
     }
 
     @Override
     public void init(GameContainer gameContainer) throws SlickException {
+        this.snowflakes = new ArrayList<>();
+        for (int i = 0; i < 500; i++) {
+            this.snowflakes.add(new Snowflake(Snowflake.SIZE.Large));
+            this.snowflakes.add(new Snowflake(Snowflake.SIZE.Medium));
+            this.snowflakes.add(new Snowflake(Snowflake.SIZE.Small));
+        }
 
     }
 
     @Override
-    public void update(GameContainer gameContainer, int i) throws SlickException {
-
+    public void update(GameContainer gameContainer, int delta) throws SlickException {
+        for (Actor actor : this.snowflakes) {
+            actor.update(delta);
+        }
     }
 
     @Override
     public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
-
+        for (Actor actor : this.snowflakes) {
+            actor.render(graphics);
+        }
     }
 
     public static void main(String[] argv) {
         try {
-            AppGameContainer container = new AppGameContainer(new ObjectsGame("Rectangle"));
+            AppGameContainer container = new AppGameContainer(new Snowworld("Snow World"));
             container.setDisplayMode(800, 600, false);
             container.start();
         } catch (SlickException e) {
             e.printStackTrace();
         }
-}}
+    }
+}
